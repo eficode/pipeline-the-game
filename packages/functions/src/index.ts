9d707fe30965d8d7b4dc5e29d0d3be9ead0cc0de
Alberto Di Gioacchino <alberto.digioacchino.93@gmail.com>
2020-12-28 18:31:25 +0100

    fix(functions-config): fix envs scripts

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,6 +1,13 @@
 import {serviceAccount} from './service-account';
 import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+});
+
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
 });
