a7392a88a41bfc9cc7c5e73db28d61ea42668506
Luca Micieli <rams23@live.it>
2020-12-31 23:37:42 +0100

    docs(app-config): add documentation to requests-status package

--- packages/game-app/src/_shared/requests-status/index.ts
+++ packages/game-app/src/_shared/requests-status/index.ts
@@ -1,3 +1,15 @@
+/**
+ * Manage centralized loading and errors for async requests.
+ *
+ * State containing loading error and success status for each request key.
+ *
+ * The package provides a saga utils that adds the request management automatically
+ * the {@link addRequestStatusManagement} wrapper.
+ *
+ * Looking at the request status is easy with the hook generator {@link createRequestHook}
+ *
+ * @packageDocumentation
+ */
 import reducer, { actions, name } from './slice';
 import { RequestsKeys } from './requestsKeys';
 import { addRequestStatusManagement } from './sagaIntegrations';
--- packages/game-app/src/_shared/requests-status/sagaIntegrations.ts
+++ packages/game-app/src/_shared/requests-status/sagaIntegrations.ts
@@ -3,6 +3,41 @@ import { call, put } from 'redux-saga/effects';
 import { actions } from './index';
 import { SagaIterator } from 'redux-saga';
 
+/**
+ *
+ * Just wrap your saga using this function when registering like this
+ *
+ * @example
+ * ```typescript
+ *
+ * function* mySaga(){
+ *   ...
+ * }
+ *
+ * yield takeLeading(actions.myAction, addRequestStatusManagement(mySaga, 'requestKey'));
+ *
+ * ```
+ *
+ * Before executing your saga the status is set to loading.
+ * If the saga throws an error it is saved into the state under the give key
+ * otherwise the request is considered successfully executed.
+ *
+ * This means that if you catch any error that must be handled with some custom
+ * code then you have to rethrow it in the standard form of {code:string;message:string;}
+ *
+ * @example
+ * ```typescript
+ * try{
+ *
+ * } catch(e){
+ *    // some custom code to handle the error or parse it
+ *    throw {code, message};
+ * }
+ * ```
+ *
+ * @param gen classical saga generator function
+ * @param requestKey the key to save request status
+ */
 export function addRequestStatusManagement<T = any, Fn extends (...args: any[]) => SagaIterator<T> = () => any>(
   gen: Fn,
   requestKey: keyof RequestsKeys,
