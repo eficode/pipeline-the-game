eed8fe856b21d7186b68541f90cd353276c2d50c
Luca Micieli <rams23@live.it>
2021-01-04 15:15:39 +0100

    test(app-signup): add redirect assertions after email verification

--- packages/game-app/cypress/integration/email_verification.spec.ts
+++ packages/game-app/cypress/integration/email_verification.spec.ts
@@ -58,9 +58,41 @@ context("Email verification", () => {
       const path = `/verify-email?${url.split('?')[1]}`
       cy.visit(path);
       cy.wait('@verify').its('response.body').should('deep.include', {email: randomEmail, emailVerified: true})
+      cy.location('pathname').should('equal', '/dashboard')
       alreadyUsedLink = path;
     });
   });
+
+  describe('it should verify email correctly and ',() => {
+
+    let verificationLink:string;
+
+    before(() => {
+      cy.clearLocalStorage()
+      cy.clearIndexedDB();
+      cy.visit(Cypress.config().baseUrl!);
+      const randomEmail = `testEmail${Math.floor(Math.random() * 1000)}@email.com`.toLocaleLowerCase();
+      cy.window().its('store').invoke('dispatch', {
+        type: 'signup/start', payload: {
+          email: randomEmail,
+          password: 'Aa1%sfesfsf',
+          repeatPassword: 'Aa1%sfesfsf',
+          role: 'endUser',
+          devOpsMaturity: 'veryImmature',
+        }
+      });
+      cy.containsTranslationOf('signup.verificationRequired.message')
+      cy.getEmailVerificationLink(randomEmail).then(url => {
+        verificationLink = `/verify-email?${url.split('?')[1]}`;
+      });
+    })
+
+    it('go to login if not authenticated', ()=>{
+      cy.visit(verificationLink);
+      cy.location('pathname').should('equal', '/login')
+    })
+
+  })
 
   it("should show invalid code error on link reuse", () => {
     cy.visit(alreadyUsedLink);
