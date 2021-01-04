e9de7508b07ad5d8546e0de018c23509c3a544e9
Alberto Di Gioacchino <alberto.digioacchino.93@gmail.com>
2021-01-04 11:40:44 +0100

    feat(app-general): improve dynamic data retrieval

--- packages/game-app/src/_shared/dynamicData/saga.ts
+++ packages/game-app/src/_shared/dynamicData/saga.ts
@@ -2,11 +2,11 @@ import { call, put, takeEvery } from 'redux-saga/effects';
 import { addRequestStatusManagement } from '@pipeline/requests-status';
 import { executeRetrieveDevOpsMaturities, executeRetrieveGameRoles } from './apis/retrieveDynamicData';
 import { actions } from './slice';
-import { SelectOption } from '@pipeline/models';
+import { DevOpsMaturitiesDoc, GameRolesDoc } from '@pipeline/common';
 
 function* retrieveGameRolesSaga() {
   try {
-    const gameRoles: SelectOption[] = yield call(executeRetrieveGameRoles);
+    const gameRoles: GameRolesDoc = yield call(executeRetrieveGameRoles);
     yield put(actions.setGameRoles(gameRoles));
   } catch (e) {
     throw e;
@@ -15,7 +15,7 @@ function* retrieveGameRolesSaga() {
 
 function* retrieveDevOpsMaturitiesSaga() {
   try {
-    const devOpsMaturities: SelectOption[] = yield call(executeRetrieveDevOpsMaturities);
+    const devOpsMaturities: DevOpsMaturitiesDoc = yield call(executeRetrieveDevOpsMaturities);
     yield put(actions.setDevOpsMaturities(devOpsMaturities));
   } catch (e) {
     throw e;
