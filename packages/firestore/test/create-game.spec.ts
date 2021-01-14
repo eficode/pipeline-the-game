ed52a3a680beee3d3307853c9d434005df9f688d
Alberto Di Gioacchino <alberto.digioacchino.93@gmail.com>
2021-01-14 23:57:34 +0100

    fix(firestore-create-game): add missing game creation rules tests

---
+++ packages/firestore/test/create-game.spec.ts
@@ -0,0 +1,189 @@
+import * as firebase from "@firebase/rules-unit-testing";
+import {getAuthedFirestore, reinitializeFirestore} from "./utils";
+import {FirebaseCollections} from '@pipeline/common/build/cjs'
+import {Game} from "@pipeline/common";
+import fb from "firebase";
+
+const PROJECT_ID = "firestore-emulator-example-" + Math.floor(Math.random() * 1000);
+
+const COVERAGE_URL = `http://${process.env.FIRESTORE_EMULATOR_HOST}/emulator/v1/projects/${PROJECT_ID}:ruleCoverage.html`;
+
+beforeEach(async () => {
+  await reinitializeFirestore(PROJECT_ID);
+});
+
+after(async () => {
+  await Promise.all(firebase.apps().map((app) => app.delete()));
+  console.log(`View firestore rule coverage information at ${COVERAGE_URL}\n`);
+});
+
+describe("Game create", () => {
+
+  it("should not allow game creation if not authenticated", async () => {
+    const db = getAuthedFirestore(PROJECT_ID, undefined);
+    const gameRef = db.collection(FirebaseCollections.Games).doc('game1');
+    await firebase.assertFails(gameRef.set({
+      scenarioTitle: 'Title',
+      scenarioContent: 'Content',
+      scenarioCardId: null,
+      facilitator: {
+        id: 'id1'
+      },
+      createdAt: fb.firestore.FieldValue.serverTimestamp(),
+      deckId: '7p5qqvE8kCV9WWysVc2n'
+    } as Game));
+  });
+
+  it("should allow game creation with correct data", async () => {
+    const userUID = 'id1';
+    const email = 'test@email.com';
+    const db = getAuthedFirestore(PROJECT_ID, {uid: userUID, email, email_verified: true});
+    const gameRef = db.collection(FirebaseCollections.Games).doc('game1');
+    await firebase.assertSucceeds(gameRef.set({
+      scenarioTitle: 'Title',
+      scenarioContent: 'Content',
+      scenarioCardId: null,
+      facilitator: {
+        id: userUID
+      },
+      createdAt: fb.firestore.FieldValue.serverTimestamp(),
+      deckId: '7p5qqvE8kCV9WWysVc2n'
+    } as Game));
+  });
+
+  it("should not allow game creation with invalid deckId", async () => {
+    const userUID = 'id1';
+    const email = 'test@email.com';
+    const db = getAuthedFirestore(PROJECT_ID, {uid: userUID, email, email_verified: true});
+    const gameRef = db.collection(FirebaseCollections.Games).doc('game1');
+    await firebase.assertFails(gameRef.set({
+      scenarioTitle: 'Title',
+      scenarioContent: 'Content',
+      scenarioCardId: null,
+      facilitator: {
+        id: userUID
+      },
+      createdAt: fb.firestore.FieldValue.serverTimestamp(),
+      deckId: 'randomDeckId'
+    } as Game));
+  });
+
+  it("should not allow game creation without all necessary keys", async () => {
+    const userUID = 'id1';
+    const email = 'test@email.com';
+    const db = getAuthedFirestore(PROJECT_ID, {uid: userUID, email, email_verified: true});
+    const gameRef = db.collection(FirebaseCollections.Games).doc('game1');
+    await firebase.assertFails(gameRef.set({
+      scenarioTitle: 'Title',
+      scenarioCardId: null,
+      facilitator: {
+        id: userUID
+      },
+      createdAt: fb.firestore.FieldValue.serverTimestamp(),
+      deckId: '7p5qqvE8kCV9WWysVc2n'
+    }));
+  });
+
+  it("should not allow game creation if the authenticated user is different from facilitator", async () => {
+    const userUID = 'id1';
+    const email = 'test@email.com';
+    const db = getAuthedFirestore(PROJECT_ID, {uid: userUID, email, email_verified: true});
+    const gameRef = db.collection(FirebaseCollections.Games).doc('game1');
+    await firebase.assertFails(gameRef.set({
+      scenarioTitle: 'Title',
+      scenarioContent: 'Content',
+      scenarioCardId: null,
+      facilitator: {
+        id: 'randomUserId'
+      },
+      createdAt: fb.firestore.FieldValue.serverTimestamp(),
+      deckId: '7p5qqvE8kCV9WWysVc2n'
+    } as Game));
+  });
+
+  it("should not allow game creation if createdAt is not server timestamp", async () => {
+    const userUID = 'id1';
+    const email = 'test@email.com';
+    const db = getAuthedFirestore(PROJECT_ID, {uid: userUID, email, email_verified: true});
+    const gameRef = db.collection(FirebaseCollections.Games).doc('game1');
+    await firebase.assertFails(gameRef.set({
+      scenarioTitle: 'Title',
+      scenarioContent: 'Content',
+      scenarioCardId: null,
+      facilitator: {
+        id: userUID
+      },
+      createdAt: fb.firestore.Timestamp.now(),
+      deckId: '7p5qqvE8kCV9WWysVc2n'
+    } as Game));
+  });
+
+  it("should not allow game creation if the scenario title is too long", async () => {
+    const userUID = 'id1';
+    const email = 'test@email.com';
+    const db = getAuthedFirestore(PROJECT_ID, {uid: userUID, email, email_verified: true});
+    const gameRef = db.collection(FirebaseCollections.Games).doc('game1');
+    await firebase.assertFails(gameRef.set({
+      scenarioTitle: 'x'.repeat(100),
+      scenarioContent: 'Content',
+      scenarioCardId: null,
+      facilitator: {
+        id: userUID
+      },
+      createdAt: fb.firestore.FieldValue.serverTimestamp(),
+      deckId: '7p5qqvE8kCV9WWysVc2n'
+    } as Game));
+  });
+
+  it("should not allow game creation if the scenario content is too long", async () => {
+    const userUID = 'id1';
+    const email = 'test@email.com';
+    const db = getAuthedFirestore(PROJECT_ID, {uid: userUID, email, email_verified: true});
+    const gameRef = db.collection(FirebaseCollections.Games).doc('game1');
+    await firebase.assertFails(gameRef.set({
+      scenarioTitle: 'Title',
+      scenarioContent: 'x'.repeat(3000),
+      scenarioCardId: null,
+      facilitator: {
+        id: userUID
+      },
+      createdAt: fb.firestore.FieldValue.serverTimestamp(),
+      deckId: '7p5qqvE8kCV9WWysVc2n'
+    } as Game));
+  });
+
+  it("should allow game creation if the scenario card id is present and coherent", async () => {
+    const userUID = 'id1';
+    const email = 'test@email.com';
+    const db = getAuthedFirestore(PROJECT_ID, {uid: userUID, email, email_verified: true});
+    const gameRef = db.collection(FirebaseCollections.Games).doc('game1');
+    await firebase.assertSucceeds(gameRef.set({
+      scenarioTitle: 'Fitness & Running Mobile App',
+      scenarioContent: 'Mobile exercise app monitors your running.\nGives feedback on lap time and coaches you towards your goals.\n• The system sends encouraging emails with weekly fitness\nsummaries.\nHandles potentially sensitive health data.\nYour competitor is a Silicon Valley startup using Python and their\ndeployment lead time is six hours.',
+      scenarioCardId: 'G5JfGVoM7SZ6jOsdjWWp',
+      facilitator: {
+        id: userUID
+      },
+      createdAt: fb.firestore.FieldValue.serverTimestamp(),
+      deckId: '7p5qqvE8kCV9WWysVc2n'
+    } as Game));
+  });
+
+  it("should not allow game creation if the scenario card id is present but not coherent", async () => {
+    const userUID = 'id1';
+    const email = 'test@email.com';
+    const db = getAuthedFirestore(PROJECT_ID, {uid: userUID, email, email_verified: true});
+    const gameRef = db.collection(FirebaseCollections.Games).doc('game1');
+    await firebase.assertFails(gameRef.set({
+      scenarioTitle: 'Fitness & Running Mobile App',
+      scenarioContent: 'random',
+      scenarioCardId: 'G5JfGVoM7SZ6jOsdjWWp',
+      facilitator: {
+        id: userUID
+      },
+      createdAt: fb.firestore.FieldValue.serverTimestamp(),
+      deckId: '7p5qqvE8kCV9WWysVc2n'
+    } as Game));
+  });
+
+});
