ce8e4443a9a609b33979ba1965014de2331f71d5
Luca Micieli <rams23@live.it>
2021-01-14 23:51:57 +0100

    ci(app-create-game): remove unused variable

--- packages/game-app/src/gameView/apis/callLoadCards.ts
+++ packages/game-app/src/gameView/apis/callLoadCards.ts
@@ -2,8 +2,6 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import { CardEntity, FirebaseCollections } from '@pipeline/common';
 
-const DEFAULT_DECK_ID = '7p5qqvE8kCV9WWysVc2n';
-
 export default async function loadCards(): Promise<CardEntity[]> {
   const cards = await firebase.firestore().collection(FirebaseCollections.Cards).get();
 
