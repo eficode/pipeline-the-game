7a5290a1c835c353b5fad96456f2fe4642a4bcd9
Alberto Di Gioacchino <alberto.digioacchino.93@gmail.com>
2021-01-15 10:09:25 +0100

    refactor(app-general): use firebase enums singular

--- packages/game-app/src/gameView/apis/callLoadCards.ts
+++ packages/game-app/src/gameView/apis/callLoadCards.ts
@@ -1,9 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import { CardEntity, FirebaseCollections } from '@pipeline/common';
+import { CardEntity, FirebaseCollection } from '@pipeline/common';
 
 export default async function loadCards(): Promise<CardEntity[]> {
-  const cards = await firebase.firestore().collection(FirebaseCollections.Cards).get();
+  const cards = await firebase.firestore().collection(FirebaseCollection.Cards).get();
 
   return cards.docs.map(d => ({ id: d.id, ...d.data() } as CardEntity));
 }
