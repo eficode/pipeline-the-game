c5b7d26284b9c46c2754bc95c7c863212733abf3
Luca Micieli <rams23@live.it>
2021-01-25 14:12:42 +0100

    fix(app-components): fix input import for storybook

--- packages/game-app/src/_shared/components/PasswordInput/PasswordInput.tsx
+++ packages/game-app/src/_shared/components/PasswordInput/PasswordInput.tsx
@@ -3,7 +3,7 @@ import IconButton from '../IconButton';
 import ErrorMessage from '../ErrorMessage';
 import Typography from '../Typography';
 import { IconDiv, InputContainer } from './PasswordInput.styled';
-import { Input } from '@pipeline/components';
+import Input from '../Input';
 
 type Props = {
   name: string;
