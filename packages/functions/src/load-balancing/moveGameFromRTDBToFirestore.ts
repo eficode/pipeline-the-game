a4be30d03c772e04b0a1007c48ee0d956ab4666f
Alberto Di Gioacchino <alberto.digioacchino.93@gmail.com>
2021-01-26 18:23:19 +0100

    feat(functions-load-balancing): add multiple game ids logic

--- packages/functions/src/load-balancing/moveGameFromRTDBToFirestore.ts
+++ packages/functions/src/load-balancing/moveGameFromRTDBToFirestore.ts
@@ -1,9 +1,11 @@
 import * as admin from "firebase-admin";
-import {FirebaseCollection, Game, RTDBPaths} from "@pipeline/common";
+import {FirebaseCollection, CardState, RTDBPaths} from "@pipeline/common";
+import {Game} from "../models/Game";
+import {RTDBGame} from "../models/RTDBGame";
 
 /**
  * It moves a game from a particular RTDB instance to Firestore.
- * Before writing to Firestore, the 'rtdbInstance' field is set to null
+ * Before writing to Firestore, the 'rtdbInstance' field is set to null and all the cards are unlocked
  *
  * @param gameId
  * @param db
@@ -11,9 +13,17 @@ import {FirebaseCollection, Game, RTDBPaths} from "@pipeline/common";
  */
 const moveGameFromRTDBToFirestore = async (gameId: string, db: FirebaseFirestore.Firestore, rtdb: admin.database.Database) => {
   const gameSnap = await rtdb.ref(`/${RTDBPaths.Games}/${gameId}`).get();
-  const game = gameSnap.val() as Game;
-  game.rtdbInstance = null;
-  await db.collection(FirebaseCollection.Games).doc(gameId).update({...game});
+  const game = gameSnap.val() as RTDBGame;
+  const cardsSnap = await rtdb.ref(`/${RTDBPaths.Cards}/${gameId}`).get();
+  let newCards = null;
+  if (cardsSnap.exists()) {
+    const cards = cardsSnap.val() as {[key: string]: CardState};
+    newCards = Object.keys(cards).reduce((acc, cardId) => {
+      acc[cardId] = {...cards[cardId], lockedBy: null};
+      return acc;
+    }, {} as {[key: string]: CardState});
+  }
+  await db.collection(FirebaseCollection.Games).doc(gameId).update({...game, rtdbInstance: null, cards: newCards} as Game);
 }
 
 export {moveGameFromRTDBToFirestore};
--- packages/functions/src/load-balancing/onOnlineGameStatusUpdate.ts
+++ packages/functions/src/load-balancing/onOnlineGameStatusUpdate.ts
@@ -1,15 +1,50 @@
 import * as functions from 'firebase-functions';
 import * as admin from "firebase-admin";
-import {FirebaseCollection, RTDBInstance, RTDBPaths, Status} from "@pipeline/common";
+import {FirebaseCollection, RTDBInstance, RTDBPaths} from "@pipeline/common";
 import {PROJECT_ID} from "../utils/rtdb";
-import FieldValue = admin.firestore.FieldValue;
 import {moveGameFromRTDBToFirestore} from "./moveGameFromRTDBToFirestore";
+import FieldValue = admin.firestore.FieldValue;
+import {Status} from "../models/Status";
 
 const db = admin.firestore();
 const logger = functions.logger;
 
 const INSTANCE_ID = `${PROJECT_ID}-default-rtdb`
 const INSTANCE_NAME = `${INSTANCE_ID}.europe-west1`
+
+async function handleMoveGame(nextStatus: Status, rtdb: admin.database.Database) {
+  for (const gameId of Object.keys(nextStatus.gameIds!)) {
+    if (nextStatus.gameIds![gameId]) {
+      const snap = await rtdb.ref(`/${RTDBPaths.Statuses}`).orderByChild(`gameIds/${gameId}`).equalTo(true).get();
+      const statuses: Status[] = [];
+      snap.forEach(s => {
+        statuses.push(s.val());
+      });
+      const onlineCount = statuses.filter((s: Status) => s.state === 'online').length;
+      logger.log(`Online user for game ${gameId}: ${onlineCount}`);
+      if (onlineCount <= 1) {
+        await moveGameFromRTDBToFirestore(gameId, db, rtdb);
+        logger.log(`Game ${gameId} moved from RTDB to Firestore`);
+      }
+    }
+  }
+}
+
+async function handleLockedCards(nextStatus: Status, rtdb: admin.database.Database, userId: string) {
+  for (const gameId of Object.keys(nextStatus.gameIds!)) {
+    if (nextStatus.gameIds![gameId]) {
+      const snap = await rtdb.ref(`/${RTDBPaths.Cards}/${gameId}`).orderByChild(`lockedBy`).equalTo(userId).get();
+      const cardRefs = [] as admin.database.Reference[];
+      snap.forEach(s => {
+        cardRefs.push(s.ref);
+      });
+      logger.log(`${cardRefs.length} cards locked by ${userId}`);
+      for (const cardRef of cardRefs) {
+        await cardRef.update({lockedBy: null});
+      }
+    }
+  }
+}
 
 /**
  * It triggers when the path /statuses/{userId} of that RTDB instance is updated.
@@ -18,11 +53,10 @@ const INSTANCE_NAME = `${INSTANCE_ID}.europe-west1`
  *  - if the new one is 'online', the correct document of Firestore, representing the RTDB instance, should be updated incrementing by +1
  *  - if the new one is 'offline', the correct document of Firestore, representing the RTDB instance, should be updated incrementing by -1
  *
- *  Next, if the new status is 'offline', an RTDB query is performed to look for the online users for that game.
- *  If more than one are found (because one is the one we were updating), this means there is still someone in the game
- *  Otherwise, we can move the game from RTDB back to Firestore
+ *  Next, if the new status is 'offline', an RTDB query is performed to look for the online users for those games.
+ *  If more than one were found (because one is the one we were updating), it means there is still someone in the game
+ *  Otherwise, we can move the game from RTDB back to Firestore, for each game.
  *
- *  TODO check if locked cards should be unlocked
  *
  */
 
@@ -37,29 +71,24 @@ export const onOnlineGameStatusUpdate = functions.database.instance(INSTANCE_ID)
 
 
     if (originalStatus.state !== nextStatus.state) {
-
-        const gameId = nextStatus.gameId as string;
-
-        logger.log(`User ${userId} from game ${gameId} going from ${originalStatus.state} to ${nextStatus.state}`);
-        const docInstanceId = instanceId.split(`${PROJECT_ID}-`)[1];
-        await db.collection(FirebaseCollection.RTDBInstances).doc(docInstanceId)
-          .update({onlineOnGameCount: nextStatus.state === 'online' ?
-                FieldValue.increment(1) as any :
-                FieldValue.increment(-1) as any,
-          } as Partial<RTDBInstance>);
-        if (nextStatus.state === 'offline') {
-            const rtdb = admin.app().database(`https://${INSTANCE_NAME}.firebasedatabase.app`);
-            const snap = await rtdb.ref(`/${RTDBPaths.Statuses}`).orderByChild('gameId').equalTo(gameId).get();
-            const statuses: Status[] = [];
-            snap.forEach(s => {
-                statuses.push(s.val());
-            });
-            const onlineCount = statuses.filter((s: Status) => s.state === 'online').length;
-            logger.log(`Online user for game ${gameId}: ${onlineCount}`);
-            if (onlineCount <= 1) {
-                await moveGameFromRTDBToFirestore(gameId, db, rtdb);
-                logger.log(`Game ${gameId} moved from RTDB to Firestore`);
-            }
+      logger.log(`User ${userId} is going from ${originalStatus.state} to ${nextStatus.state}`);
+      const docInstanceId = instanceId.split(`${PROJECT_ID}-`)[1];
+      await db.collection(FirebaseCollection.RTDBInstances).doc(docInstanceId)
+        .update({
+          onlineOnGameCount: nextStatus.state === 'online' ?
+            FieldValue.increment(1) as any :
+            FieldValue.increment(-1) as any,
+        } as Partial<RTDBInstance>);
+      if (nextStatus.state === 'offline') {
+        const rtdb = admin.app().database(`https://${INSTANCE_NAME}.firebasedatabase.app`);
+        if (nextStatus.gameIds) {
+          await handleLockedCards(nextStatus, rtdb, userId);
+          logger.log('Locked cards handled');
+          await handleMoveGame(nextStatus, rtdb);
+          logger.log('Locked cards handled');
+        }  else {
+          logger.log('No games');
         }
+      }
     }
   });
