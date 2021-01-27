7041a17a9a6746d333bc7d18957da25648e62d52
Luca Micieli <rams23@live.it>
2021-01-27 18:41:45 +0100

    chore(all-general): update function bundle with local dependecies

---
+++ scripts/post-deploy.js
@@ -0,0 +1,18 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+(async () => {
+  const root = path.resolve(__dirname, '..');
+  const functionFolder = path.join(root, `packages/functions`);
+  const functionsPackageJsonPath = path.join(functionFolder, "package.json");
+  const appPackageJsonPath = path.join(root, "packages/game-app/package.json");
+
+  const packageJsonFunctions = require(functionsPackageJsonPath);
+  const packageJsonApp = require(appPackageJsonPath);
+
+  await fs.remove(path.join(functionFolder,`_packages_copy`));
+
+  packageJsonFunctions.dependencies["@pipeline/common"] = packageJsonApp.dependencies["@pipeline/common"];
+
+  await fs.writeFile(functionsPackageJsonPath, JSON.stringify(packageJsonFunctions, null, 2));
+})();
