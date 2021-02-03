65742daf731953222e827df7022b0e4fdde03747
Alberto Di Gioacchino <alberto.digioacchino.93@gmail.com>
2021-02-03 16:18:57 +0100

    ci(all-general): fix load rules script and steps

--- scripts/load-rules-to-rtdb-instances.js
+++ scripts/load-rules-to-rtdb-instances.js
@@ -7,7 +7,7 @@ const databaseRules = fs.readFileSync("./packages/database/database.rules.json")
 
 function run(cmd) {
   return new Promise((resolve, reject) => {
-    childProcess.exec(cmd, (error, stdout, stderr) => {
+    childProcess.exec(cmd, {env: process.env}, (error, stdout, stderr) => {
       if (error) return reject(error)
       if (stderr) return reject(stderr)
       resolve(stdout)
@@ -27,9 +27,8 @@ async function loadRulesToRTDBInstances(app) {
   } else {
     const listDatabases = firebaseTools.database.instances.list
     const databases = await listDatabases();
-    await run(`firebase target:clear database main`);
-    await run(`firebase target:apply database main ${databases.map(d => d.instance).join(' ')}`);
-    await run(`firebase deploy --only database`);
+    await run(`firebase target:clear database main --project $FIREBASE_PROJECT_ID`);
+    await run(`firebase target:apply database main ${databases.map(d => d.instance).join(' ')} --project $FIREBASE_PROJECT_ID`);
   }
 }
 
