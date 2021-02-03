e36e2e499f9d966c071ca9e907d6384db3b3cef1
Alberto Di Gioacchino <alberto.digioacchino.93@gmail.com>
2021-02-03 16:10:30 +0100

    ci(all-general): fix load rules script

--- scripts/load-rules-to-rtdb-instances.js
+++ scripts/load-rules-to-rtdb-instances.js
@@ -1,23 +1,35 @@
-const admin = require('firebase-admin');
 const fs = require('fs');
 const fetch = require('node-fetch');
+const childProcess = require('child_process');
+const firebaseTools = require('firebase-tools');
 
 const databaseRules = fs.readFileSync("./packages/database/database.rules.json");
 
+function run(cmd) {
+  return new Promise((resolve, reject) => {
+    childProcess.exec(cmd, (error, stdout, stderr) => {
+      if (error) return reject(error)
+      if (stderr) return reject(stderr)
+      resolve(stdout)
+    })
+  })
+}
+
 async function loadRulesToRTDBInstances(app) {
-  const firestore = app.firestore();
-  const rtdbInstancesCollectionQuery = await firestore.collection('rtdbInstances').get();
-  for (const rtdbDoc of rtdbInstancesCollectionQuery.docs) {
-    const rtdbId = rtdbDoc.id;
-    const rtdbData = rtdbDoc.data();
-    if (process.env.FIREBASE_DATABASE_EMULATOR_HOST) {
+  if (process.env.FIREBASE_DATABASE_EMULATOR_HOST) {
+    const firestore = app.firestore();
+    const rtdbInstancesCollectionQuery = await firestore.collection('rtdbInstances').get();
+    for (const rtdbDoc of rtdbInstancesCollectionQuery.docs) {
+      const rtdbId = rtdbDoc.id;
       const databaseURL = `http://${process.env.FIREBASE_DATABASE_EMULATOR_HOST}/.settings/rules.json?ns=${process.env.GCLOUD_PROJECT}-${rtdbId}&access_token=`;
       await fetch(databaseURL, {method: 'PUT', body: databaseRules})
-    } else {
-      const databaseURL = `http://${process.env.GCLOUD_PROJECT}-${rtdbId}.${rtdbData.region}`;
-      const rtdbInstance = admin.app().database(databaseURL);
-      await rtdbInstance.setRules(databaseRules);
     }
+  } else {
+    const listDatabases = firebaseTools.database.instances.list
+    const databases = await listDatabases();
+    await run(`firebase target:clear database main`);
+    await run(`firebase target:apply database main ${databases.map(d => d.instance).join(' ')}`);
+    await run(`firebase deploy --only database`);
   }
 }
 
