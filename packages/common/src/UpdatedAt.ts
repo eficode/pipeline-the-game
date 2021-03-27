6ba5ab3aeda81507945769d47c9104f783430c67
Luca Micieli <rams23@live.it>
2021-03-27 19:26:53 +0100

    docs(common-general): improve documentation

--- packages/common/src/UpdatedAt.ts
+++ packages/common/src/UpdatedAt.ts
@@ -1,4 +1,9 @@
-
+/**
+ * Generics are used because of the different model for the admin sdk
+ * and dhe client sdk regarding the Timestamp and serverTimestamp
+ * object. The client and the server packages can share the data model
+ * but they have to specify the timestamp interface of the firebase sdk
+ */
 export interface UpdatedAt<T, F> {
   updatedAt: T | F;
 }
