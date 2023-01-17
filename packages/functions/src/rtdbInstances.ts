e4ff0367679ced057797c799386b45b18145133a
pattkrajnik <patryk.krajnik@eficode.com>
2023-01-17 16:57:37 +0100

    updated db adress

--- packages/functions/src/rtdbInstances.ts
+++ packages/functions/src/rtdbInstances.ts
@@ -1,12 +1,12 @@
 const rtdbInstancesUrl = [
   {
-    url: 'https://pipeline-game-dev-default-rtdb.firebasedatabase.app',
-    name: 'pipeline-game-dev-default-rtdb',
+    url: 'https://pipeline-game-development-default-rtdb.firebasedatabase.app',
+    name: 'pipeline-game-development-default-rtdb',
     id: 'default-rtdb',
   },
   {
-    url: 'https://pipeline-game-dev-instance-1.firebasedatabase.app',
-    name: 'pipeline-game-dev-instance-1',
+    url: 'https://pipeline-game-development-instance-1.firebasedatabase.app',
+    name: 'pipeline-game-development-instance-1',
     id: 'instance-1',
   },
 ];
